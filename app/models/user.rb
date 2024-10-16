6d420c6722588e6dacae1a24866afa5d722905a9
likapantrip <likapantrip@gmail.com>
2024-10-17 00:44:01 +0900

    ユーザー管理機能の実装
    下記を実装
    ・モデルのバリデーション
    ・新規登録機能
    ・ログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-ゖァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday, presence: true
 end
