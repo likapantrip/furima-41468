0604740b4aeee89530599c1b5738fce43524573a
likapantrip <likapantrip@gmail.com>
2024-10-17 16:47:58 +0900

    itemモデルのバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  validates :name,            presence: true
+  validates :content,         presence: true
+  validates :category_id,     presence: true
+  validates :condition_id,    presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id,   presence: true
+  validates :shipping_day_id, presence: true
+  validates :item_price,      presence: true
+
+  belongs_to :user
+  
 end
+
