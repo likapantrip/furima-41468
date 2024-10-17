65a12d0181f4b6b6d82479f7f12e3fb0237cfe96
likapantrip <likapantrip@gmail.com>
2024-10-17 15:21:17 +0900

    指摘内容の修正
    テスト用インスタンスのパスワードに下記を追記
    +'1a'

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Internet.password(min_length: 6)+'1a'}
     password_confirmation {password}
     nickname              {Faker::Internet.username}
     last_name             {"田中"}
