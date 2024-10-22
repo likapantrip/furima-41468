6c44de9cf677dd33c5c2bb69606a3e6578bb2e85
likapantrip <likapantrip@gmail.com>
2024-10-22 16:43:39 +0900

    Formオブジェクト「OrderShip」の作成

---
+++ app/models/order_ship.rb
@@ -0,0 +1,26 @@
+class OrderShip
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :street, :building, :phone_number, :order_id
+
+  # Orderモデルのバリデーション
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+  end
+
+  # Shipモデルのバリデーション
+  with_options presence: true do
+    validates :post_code
+    validates :prefecture_id
+    validates :city
+    validates :street
+    validates :phone_number
+    validates :order
+  end
+
+  # 保存処理
+  def save
+    order = Order.create(user: user_id, item: item_id)
+    Ship.create(post_code: post_code, prefecture: prefecture, city: city, street: street, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
