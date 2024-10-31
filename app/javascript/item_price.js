4374fe43722687da5742264fb742a63936cabe55
likapantrip <likapantrip@gmail.com>
2024-10-31 10:57:59 +0900

    コンソール上のエラー解消
    JavaScriptを修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,6 @@
 const price = () => {
   const priceInput = document.getElementById("item-price");
+  if (!priceInput){ return false;}
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
@@ -11,4 +12,3 @@ const price = () => {
 
 window.addEventListener("turbo:load", price);
 window.addEventListener("turbo:render", price);
-
