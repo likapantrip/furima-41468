ca436e9124f8ff1aceb1665a20b0d4b704056389
likapantrip <likapantrip@gmail.com>
2024-12-11 07:27:41 +0900

    マイページの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @items = Item.where(user_id: params[:id]).order('created_at DESC')
 
     Payjp.api_key = ENV['PAYJP_SECRET_KEY'] # 環境変数を読み込む
     @user_card = Card.find_by(user_id: current_user)
